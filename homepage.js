80743e89257263f6e2523d8ba778ab9b93b1726d
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-29 11:40:47 -0400

    am push. Attempted 2 ways to carousel/slideshow. pictures positioned correctly but cant get it all to work

--- homepage.js
+++ homepage.js
@@ -8,9 +8,48 @@ $(document).ready(function(){
     })
 })
 
+// on scroll to .project1 start displying images at a speed of x
+//carousel as per class corgi example
+//not working
 
+// let currentImgIndex = 0;
+// let numOfImages = $('.carousel1-images').children().length-1
+// // let time = 3000;
 
+// $('.project1').on('scroll', () => {
+//     $('.carousel1-images').children().eq(currentImgIndex).css('display', 'none')
+//     if(currentImgIndex < numOfImages){
+//         currentImgIndex++
+//     }else{
+//         currentImgIndex = 0
+//     }
+//     $('.carousel1-images').children().eq(currentImgIndex).css('display', 'block')
+// })
 
+// window.onload = ("nameoffunction()", time))
+
+// slideshow attempt2
+
+let i = 0;
+let images = [];
+let time = 3000;
+
+//image list
+images[0] = "https://i.imgur.com/e09tUM9.jpg";
+images[1] = "https://i.imgur.com/onafOnn.jpg";
+images[2] = "https://i.imgur.com/j6RbFqx.jpg";
+
+//change image
+
+function changeImg(){
+    document.slide.src = images[i];
+    if(i < images.length -1){
+        i++;
+    } else{
+        i=0;
+    }
+    setTimeout("changeImg()", time);
+}
 
 
 

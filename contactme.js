b29ef343744f5d546b0935e250be671ed9da48c4
Carolina Villalobos <cvillal@ncsu.edu>
2022-08-01 15:07:41 -0400

    link buttons adjusted again for smaller screens

--- contactme.js
+++ contactme.js
@@ -3,7 +3,7 @@ $(() => {
     $('.submenu').hide()
     
     $(document).ready(function(){
-        setInterval(changeImg, 1000)
+        
         $('#projects').hover(function(){
         $('.submenu').toggle();
         })
